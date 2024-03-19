0f2b2d9414dca6e74cc4bd5a987b3bb1dfb148d3
MatteoMassa16 <156576643+MatteoMassa16@users.noreply.github.com>
2024-03-19 10:42:12 +0100

    Add files via upload

---
+++ main_tp2.adb
@@ -0,0 +1,15 @@
+WITH Ada.Text_IO,Ada.Integer_Text_IO,Ada.Float_Text_IO,outils;
+USE Ada.Text_IO,Ada.Integer_Text_IO,Ada.Float_Text_IO,Outils;
+
+PROCEDURE Main_TP2 IS
+   A : Arbre;
+
+BEGIN
+
+
+
+
+   SaisieArb(A);
+   Affichage(A);
+
+End Main_TP2;
