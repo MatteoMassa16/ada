9c57c70f1e072cc02ecb57593a32aba6812b281e
Mattéo_ms16 <156576643+MatteoMassa16@users.noreply.github.com>
2024-04-05 16:47:01 +0200

    Update Consultant.ads

--- Consultant.ads
+++ Consultant.ads
@@ -1,46 +1,43 @@
 WITH Outils, ada.Text_IO, ada.Characters ;
-USE Outils, ada.Text_IO, ada.Character ;
+USE Outils, ada.Text_IO, ada.Characters ;
 
 Package Consultant is
 
-   TYPE T_Consultant IS RECORD --définition consultant
+   TYPE T_Cslt IS RECORD --définition consultant
 
       Nom: T_Mot :=(OTHERS =>' ');
 
       Prenom: T_Mot:=(OTHERS =>' ');
 
-      Ville: character;
-
-      Visite: T_pVisite;
+      Ville: T_Mot;
+         
+      --      Visite: T_pVisite;
 
       Frais_Dpl: Float;
 
 
       END RECORD;
 
- T_ListeConsultant is record
-	Consultant : T_consultant;
-	suiv : T_ptr;
- END RECORD;
+
 
    TYPE T_Liste_Cslt;
 
    TYPE T_Ptr_Cslt IS ACCESS T_Liste_Cslt;
 
-   TYPE T_ListeConsultant IS RECORD
+   TYPE T_Liste_Cslt IS RECORD
 
-      Cslt: T_Consultant;
+      Cslt:T_Cslt;
 
       Suiv: T_Ptr_Cslt;
 
    END RECORD;
 
 
-   PROCEDURE Initialisation_Cslt (C: out T_Cslt; Tete: in out T_Ptr_Cslt) ;
-   PROCEDURE Supp_Cslt (L: in out T_Ptr_Cslt; ok: out boolean; Cslt: T_Cslt) ;
-   PROCEDURE Ajout_Cslt (C: out T_Cslt; tete: IN OUT T_Ptr_Cslt) ;
-   PROCEDURE Saisie_Cslt (C: OUT T_Cslt; T: in out T_TabVisite ;
 
+   PROCEDURE Initialisation_Cslt (C: out T_Cslt; Tete: in out T_Ptr_Cslt) ;
+--   PROCEDURE Supp_Cslt (L: in out T_Ptr_Cslt; ok: out boolean; Cslt: T_Cslt) ;
+   PROCEDURE Ajout_Cslt (C: T_Cslt; tete: IN OUT T_Ptr_Cslt) ;
+--   PROCEDURE Saisie_Cslt (C: OUT T_Cslt; T: in out T_TabVisite) ;
 
 END Consultant;
 
