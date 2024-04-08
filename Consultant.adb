c006d03ab7ee8f19cb2def9b80ca8a718402a6a0
Mattéo_ms16 <156576643+MatteoMassa16@users.noreply.github.com>
2024-04-08 15:20:07 +0200

    Update Consultant.adb

--- Consultant.adb
+++ Consultant.adb
@@ -77,3 +77,71 @@ END Initialisation_Cslt;
       END IF;
    END Ajout_Cslt;
 
+
+----------------------------------------------------------------------------------------
+   
+--  PROCEDURE MAJ_Cslt (tete: IN OUT T_Ptr_Cslt; T: OUT T_Cslt; J: Jour) IS
+--   BEGIN
+--      IF J= T_semaine'value("lundi") THEN
+--         IF tete/=NULL THEN
+--            tete.C.Cslt:= tete.C.Cslt_Suiv;
+--            initiaTab(T);
+--            tete.C.Cslt_Suiv:=T;
+--         END IF;
+--      END IF;
+
+--   END MAJ_Cslt ;
+----------------------------------------------------------------------------------------
+
+
+--   PROCEDURE Affichage_Cslt (tete: T_Ptr_Cslt; C: OUT T_Cslt) IS
+--      ID: T_Ptr_BS:=tete;
+--      k: integer;
+--   BEGIN
+
+--      FOR i IN C.Nom'RANGE LOOP
+--         C.Nom(i) :=' ';
+--      END LOOP;
+
+--      FOR i IN C.Prenom'RANGE LOOP
+--         C.Prenom(i) :=' ';
+--      END LOOP;
+
+--      put("Saisir le nom du consultant: ");
+--      get_line(C.Nom,k);
+--      put("Saisir le prenom du consultant: ");
+--      get_line(C.Prenom,k);
+
+--      WHILE ID/=null loop
+--         IF ID.C.Nom = C.Nom AND ID.BS.Prenom = C.Prenom THEN
+
+--            FOR i IN T_semaine LOOP
+--               new_line;
+--               put(T_semaine'image(i));
+--               new_line;
+--               FOR j IN T_creneau LOOP
+--                  put(T_creneau'image(j));
+--                  put(": ");
+--                  put(ID.C. -- VISITE?(i,j));
+--                  new_line;
+--               END LOOP;
+--            END LOOP;
+--         END IF;
+--         ID:=ID.suiv;
+--      END LOOP;
+--   END Affichage_Cslt;
+   
+----------------------------------------------------------------------------------------
+   
+   --PROCEDURE RemiseZ (L: IN OUT T_Ptr_Cslt) IS --pour chaque debut de semaine
+   --BEGIN
+   --      IF (L/= NULL) THEN
+   --         IF L.Jour= Lundi THEN
+   --            L.C.F_Depla := 0;
+   --            RemiseZ (L, Deplacement);
+   --         ELSE
+   --            RemiseZ (L.Suiv, Deplacement);
+   --         END IF;
+   --      END IF;
+   --END RemiseZ ;
+END Consultant ; 
